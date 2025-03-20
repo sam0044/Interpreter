746a36d9e1399b4f1ce7621c1693b851886b2147
Sam Goyal <samgoyal0044@gmail.com>
2025-03-20 15:39:45 -0700

    added "pretty printer" code to print the expressions in a readable way and test function

---
+++ src/printer/printer.c
@@ -0,0 +1,57 @@
+#include "printer.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void printExpr(Expr* expr){
+    switch(expr->type){
+        case EXPR_BINARY:
+            printf("(");
+            printf(" %s ", expr->expression.binary.oper.lexeme);
+            printValue(expr->expression.binary.left);
+            printValue(expr->expression.binary.right);
+            printf(")");
+            break;
+        case EXPR_UNARY:
+            printf("(");
+            printf("%s ", expr->expression.unary.oper.lexeme);
+            printValue(expr->expression.unary.right);
+            printf(")");
+            break;
+        case EXPR_GROUPING:
+            printf("(group ");
+            printValue(expr->expression.grouping.expression);
+            printf(")");
+            break;
+        case EXPR_LITERAL:
+            switch(expr->expression.literal.type){
+                case LITERAL_INTEGER:
+                    printf("%d", expr->expression.literal.value.number.integer);
+                    break;
+                case LITERAL_FLOAT:
+                    printf("%f", expr->expression.literal.value.number.floating);
+                    break;
+                case LITERAL_STRING:
+                    printf("\"%s\"", expr->expression.literal.value.string);
+                    break;
+                case LITERAL_BOOLEAN:
+                    printf("%s", (expr->expression.literal.value.boolean) ? "true" : "false");
+                    break;
+                case LITERAL_NIL:
+                    printf("nil");
+                    break;
+            }
+            break;
+        default:
+            fprintf(stderr, "Invalid expression type");
+            break;
+    }
+}
+
+static void printValue(Expr* expr){
+    if(expr){
+        printExpr(expr);
+    }
+    else{
+        fprintf(stderr, "Invalid expression type");
+    }
+}
---
+++ src/printer/printer.h
@@ -0,0 +1,9 @@
+#ifndef PRINTER_H
+#define PRINTER_H
+
+#include "../expression/expression.h"
+
+void printExpr(Expr* expr);
+static void printValue(Expr* expr);
+
+#endif
